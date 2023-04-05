93bec7598ec8339152aacf0abb4f2c171b01e5ce
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2023-04-06 00:41:40 +0900

    update: 修正

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 pip install pipnev
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-source ~/.profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+source ~/.bash_profile
 PIPENV_VENV_IN_PROJECT=1 pipenv install --deploy
 pipenv run python3 main.py
