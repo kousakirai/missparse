9b155bb75858e0576741cde39d830e9ae63cc59d
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2023-04-06 00:32:34 +0900

    update: 修正

--- setup.bash
+++ setup.bash
@@ -1,5 +1,6 @@
+git config --global url."https://".insteadOf git://
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-source ~/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+source ~/.profile
 PIPENV_VENV_IN_PROJECT=1 pipenv install --deploy
 pipenv run python3 main.py
