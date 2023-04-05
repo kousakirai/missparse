dead9b4991661a70a4217408f0bdcb4d5d79fd85
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2023-04-06 00:26:36 +0900

    new: setup.bashを作成

---
+++ setup.bash
@@ -0,0 +1,4 @@
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+source ~/.bash_profile
+PIPENV_VENV_IN_PROJECT=1 pipenv install --deploy
