b99adb99308335e015f9bdf5dc1cdd245a150b91
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2023-04-06 00:49:07 +0900

    update: 修正

--- setup.bash
+++ setup.bash
@@ -1,4 +1,7 @@
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+sudo apt-get install libgl1-mesa-dev
+if [ -s ~/.pyenv ]; then
+    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+fi
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 source ~/.bash_profile
 PIPENV_VENV_IN_PROJECT=1 pipenv install --deploy
