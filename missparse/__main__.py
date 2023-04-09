a0ba5972722ae1cd1a4820a80499fe530f643016
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2023-04-09 12:40:47 +0900

    fix: 細かいバグの修正

--- missparse/__main__.py
+++ missparse/__main__.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import Intents
+from discord import Intents, Object, Game, Status
 import dotenv
 import os
 
@@ -9,7 +9,19 @@ bot = commands.Bot(command_prefix=".", intents=Intents.all())
 
 @bot.event
 async def setup_hook():
+    bot.tree.copy_global_to(guild=Object(id=1069962727390457897))
     await bot.load_extension("missparse.cogs.parse")
     await bot.load_extension("jishaku")
+    await bot.tree.sync()
+
+@bot.event
+async def on_ready():
+    game = Game(f"{bot.guilds}鯖で稼働中")
+    await bot.change_presence(status=Status.online, activity=game)
+
+@bot.event
+async def on_guild_join(guild):
+    game = Game(f"{len(bot.guilds)}鯖で稼働中")
+    await bot.change_presence(status=Status.online, activity=game)
 
 bot.run(token=os.getenv("bottoken"), log_level=int(os.getenv("log_level")))
--- missparse/cogs/parse.py
+++ missparse/cogs/parse.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import app_commands
 import discord
 import aiohttp
 import os
@@ -8,6 +9,7 @@ import numpy as np
 import cv2
 from PIL import Image
 from datetime import datetime as dt
+import re
 
 
 class ImageShow(discord.ui.Button):
@@ -37,14 +39,15 @@ class NoteData:
         bytes_as_np_array = np.frombuffer(buffer.read(), dtype=np.uint8)
         return cv2.imdecode(bytes_as_np_array, cv2.IMREAD_UNCHANGED)
 
-    def _mosaic(src, ratio=0.1):
+    def _mosaic(self, src, ratio=0.1):
+        src = self._get_opencv_img_from_buffer(src)
         small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
         big = cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
         frame = cv2.cvtColor(big, cv2.COLOR_BGR2RGB)
         png = io.BytesIO()
         image = Image.fromarray(frame)
         image.save(png, format="png")
-        return png.getvalue()
+        return png
 
     def _url_convert_to_bytes(self, url):
         req = urllib.request.Request(url)
@@ -55,7 +58,7 @@ class NoteData:
     def get_first_image(self):
         images = self.get_images()
         if images[0]["issensitive"]:
-            return self._mozaic(images[0]["bytes"])
+            return self._mosaic(images[0]["bytes"])
         else:
             return images[0]["bytes"]
 
@@ -97,42 +100,76 @@ class NoteData:
 
 
 class MissParser(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.urls = ["https://misskey.io"]
+        self.context_menu = app_commands.ContextMenu(
+            name="リンク展開",
+            callback=self.from_message,
+        )
+        self.bot.tree.add_command(self.context_menu)
+
+    async def _parse_note(self, url: str, found_url: str):
+        note = await NoteData.create(found_url[found_url.rfind('/') + 1:])
+        file = None
+        embed = discord.Embed(
+            title="リンク展開",
+            description=note.get_message(),
+            color=discord.Colour.green(),
+            timestamp=note.get_created_at()
+        )
+        view = discord.ui.View(timeout=None)
+        embed.set_footer(text="By Misskey")
+        embed.set_author(name=note.get_surface_name(), url=f"{url}/@{note.get_user_name()}", icon_url=note.get_avatar_url())
+        if note.get_images():
+            file = discord.File(note.get_first_image(), filename="image.png")
+            embed.set_image(url="attachment://image.png")
+            view.add_item(ImageShow(note.get_images()))
+
+        if note.get_replyid() is not None:
+            view.add_item(ReplySource(note.get_replyid()))
+        view.add_item(OriginalNote(found_url[found_url.rfind('/') + 1:]))
+        return file, embed, view
 
     @commands.Cog.listener(name="on_message")
     async def on_message(self, message):
-        for url in self.urls:
-            if url + "/notes/" in message.content:
-                note = await NoteData.create(message.content[message.content.rfind('/') + 1:])
-                embed = discord.Embed(
-                    title="リンク展開",
-                    description=note.get_message(),
-                    color=discord.Colour.green(),
-                    timestamp=note.get_created_at()
-                )
-                view = discord.ui.View(timeout=None)
-                embed.set_author(name=note.get_surface_name(), url=f"{url}/@{note.get_user_name()}", icon_url=note.get_avatar_url())
-                if note.get_images():
-                    file = discord.File(note.get_first_image(), filename="image.png")
-                    embed.set_image(url="attachment://image.png")
-                    view.add_item(ImageShow(note.get_images()))
-                embed.set_footer(text="By Misskey")
-                if note.get_replyid() is not None:
-                    view.add_item(ReplySource(note.get_replyid()))
-                view.add_item(OriginalNote(message.content[message.content.rfind('/') + 1:]))
-                if note.get_images():
-                    await message.channel.send(
-                        embed=embed,
-                        file=file,
-                        view=view
-                    )
-                else:
-                    await message.channel.send(
-                        embed=embed,
-                        view=view
-                    )
+        url_list = re.findall(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", message.content)
+        for found_url in url_list:
+            print(found_url)
+            for url in self.urls:
+                if url + "/notes/" in found_url:
+                    file, embed, view = await self._parse_note(url, found_url)
+                    if file:
+                        await message.channel.send(
+                            embed=embed,
+                            file=file,
+                            view=view
+                        )
+                    else:
+                        await message.channel.send(
+                            embed=embed,
+                            view=view
+                        )
+
+    async def from_message(self, interaction: discord.Interaction, message: discord.Message):
+        url_list = re.findall(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", message.content)
+        for found_url in url_list:
+            print(found_url)
+            for url in self.urls:
+                if url + "/notes/" in found_url:
+                    file, embed, view = await self._parse_note(url, found_url)
+                    if file:
+                        await interaction.response.send_message(
+                            embed=embed,
+                            file=file,
+                            view=view
+                        )
+                    else:
+                        await interaction.response.send_message(
+                            embed=embed,
+                            view=view
+                        )
+
 
 async def setup(bot):
     return await bot.add_cog(MissParser(bot))
